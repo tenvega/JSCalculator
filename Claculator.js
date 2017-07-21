545c41274fe6981fcd1f18e865d7562f6f2bc7e3
Enoc <etorra@gmail.com>
2017-07-20 22:52:47 -0400

    commit

---
+++ Claculator.js
@@ -0,0 +1,25 @@
+function math(value)
+
+{
+
+  document.getElementById("d").value += value;
+
+}
+
+
+
+function e()
+
+{
+
+  document.getElementById("d").value = eval(document.getElementById("d").value);
+
+}
+
+
+function c()
+
+{
+
+  document.getElementById("d").value = '';
+}
